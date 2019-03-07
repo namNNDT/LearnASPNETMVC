48d61fd7dbf040620df3b5d408e30b41dcc45ffa
Miuseorin <dacvutinhyeumanga@gmail.com>
2019-03-07 16:54:38 +0700

    Add new route Welcome

--- WebApplication/WebApplication/App_Start/RouteConfig.cs
+++ WebApplication/WebApplication/App_Start/RouteConfig.cs
@@ -13,6 +13,9 @@ namespace WebApplication
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Welcome ", "Welcome/", new { controller = "Home", action = " Index" });
+
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
